bd15e806ca3168a1989df752a4165d105bbcadb8
Divyam Singh <divyamsingh1918@gmail.com>
2026-07-30 17:17:49 +0530

    setup: basic server setup

--- apps/hooks-api/index.ts
+++ apps/hooks-api/index.ts
@@ -1,21 +1,23 @@
-import express,{Request,Response} from "express"
-import dotenv from "dotenv"
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+// import logger from "./config/logger"
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+const port = process.env.PORT;
 
+app.get("/health", (req: Request, res: Response) => {
+  return res.json({
+    success: true,
+    message: "Hooks api working",
+  });
+});
 
+app.listen(port, () => {
+  console.log("Hooks api running on ", port);
+});
 
-const app = express()
-dotenv.config()
-app.use(express.json())
-const port = process.env.PORT
-
-
-app.get("/health",(req:Request,res:Response)=>{
-    return res.json({
-        success:true,
-        message:"Hooks api working"
-    })
-})
-
-app.listen(port,()=>{
-    console.log("Hooks api running on ",port)
-})
