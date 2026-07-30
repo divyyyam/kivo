4c261a347af1051956c2a3542dc53aad8fb59565
Divyam Singh <divyamsingh1918@gmail.com>
2026-07-31 00:14:49 +0530

    add: config changes for build, dev and start scripts

--- apps/processor/index.ts
+++ apps/processor/index.ts
@@ -1,3 +1,17 @@
 import express from "express"
 import cors from "cors"
 import helmet from "helmet"
+import * as dotenv from 'dotenv';
+import {createLogger} from "@repo/logger"
+
+const app = express()
+dotenv.config()
+const port = process.env.PORT || 4000
+app.use(express.json())
+const logger = createLogger({
+    name:"processor"
+})
+
+app.listen(port,()=>{
+    logger.info("Processor initialized")
+})
